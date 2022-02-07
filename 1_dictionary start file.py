bb5395a7a21a43c071d701752dc74f0cce0bf5ef
Josh McFarland <josh_mcfarland1@baylor.edu>
2022-02-07 15:43:40 -0600

    Initial Commit

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -1,20 +1,33 @@
+from cmath import phase
+from os import remove
+
+
 phonebook = {'Chris':'555−1111',
              'Katie':'555−2222',
              'Joanne':'555−3333'}
+
+"""
 
 print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+print(phonebook)
+print(type(phonebook))
 
+phone = phonebook['Chris']
 
+print(phone)
 
+print(len(phonebook))
+
+mydictionary = dict(m=8,n=9)
+
+print(mydictionary)
 
 print()
 print('*****  end section 1 ********')
 print()
-
-
 
 
 
@@ -26,8 +39,12 @@ print('*****  start section 2 - search dictionary ********')
 print()
 
 
+name = 'Chris'
 
-
+if name in phonebook:
+    print(phonebook[name])
+else:
+    print(name, "is not in the phonebook")
 
 
 print()
@@ -44,9 +61,11 @@ print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
+print(phonebook)
+phonebook['chris'] = '555-0123'
 
-
-
+phonebook['Chris'] = '555-4444'
+print(phonebook)
 
 print()
 print('*****  end section 3 ********')
@@ -61,8 +80,8 @@ print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
-
-
+del phonebook["Chris"]
+print(phonebook)
 
 
 print()
@@ -72,14 +91,13 @@ print()
 
 
 
-
-
 print()
 print('*****  start section 5 - iterate through keys ********')
 print()
 
-
-
+for key in phonebook:
+    print(key)
+    print(phonebook[key])
 
 print()
 print('*****  end section 5 ********')
@@ -89,12 +107,12 @@ print()
 
 
 
-
 print()
 print('*****  start section 6 - iterate through values  ********')
 print()
 
-
+for value in phonebook.values():
+    print(value)
 
 
 print()
@@ -107,14 +125,16 @@ print()
 
 
 
-
 print()
 print('*****  start section 7 - iterate through both key and value pair********')
 print()
 
+for phonebook_tuple in phonebook.items():
+    print(phonebook_tuple)
 
-
-
+for key, value in phonebook.items():
+    print(key)
+    print(value)
 
 print()
 print('*****  end section 7 ********')
@@ -128,14 +148,75 @@ print()
 
 
 print()
-print('*****  start section 8 - using random and converting to list ********')
+print('*****  start section 8 - using get and clear********')
 print()
 
+phone = phonebook.get('Chris', 'key not found')
+print(phone)
 
-
+phonebook.clear()
+print(phonebook)
 
 print()
 print('*****  end section 8 ********')
+print()
+
+
+
+
+
+
+
+print()
+print('*****  start section 9 - using pop method********')
+print()
+
+remove = phonebook.pop('Chris', 'not found')
+print(remove)
+
+print(phonebook)
+
+print()
+print('*****  end section 9 ********')
+print()
+
+
+
+
+
+
+
+
+print()
+print('*****  start section 10 - using popitem********')
+print()
+
+a = phonebook.popitem()
+print(a)
+print(phonebook)
+
+print()
+print('*****  end section 10 ********')
+print()
+
+
+
+"""
+
+
+
+
+import random
+print()
+print('*****  start section 11 - using random and converting to list ********')
+print()
+
+phone = phonebook[random.choice(list(phonebook))]
+
+print(phone)
+
+print()
+print('*****  end section 11 ********')
 print()
 
 
--- 2_simple_exercises.py
+++ 2_simple_exercises.py
@@ -13,14 +13,14 @@ sampleDict = {
    }
 }
 
-
+print(sampleDict)
 
 
 # 2) Add 2 inches to the son's height.
 
 dict={"son's name": "Lucas", "son's eyes": "green", "son's height": 32, "son's weight": 25}
 
-
+print(dict["son's height"] + 2)
 
 
 
